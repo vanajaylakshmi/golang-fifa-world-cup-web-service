db5bd539e6a59a829c2fee3d098c29bae6139482
vanajaylakshmi <76813206+vanajaylakshmi@users.noreply.github.com>
2021-01-01 16:56:41 +0530

    Update handlers.go

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -6,12 +6,29 @@ import (
 
 // RootHandler returns an empty body status code
 func RootHandler(res http.ResponseWriter, req *http.Request) {
-
+	res.WriteHeader(http.StatusNoContent)
 }
 
 // ListWinners returns winners from the list
 func ListWinners(res http.ResponseWriter, req *http.Request) {
 
+	year := req.URL.Query().Get("year")
+	if year == "" {
+		res.Header().Set("Content-Type", "application/json")
+		winners, err := data.ListAllJSON()
+		if err != nil {
+			res.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		res.Write(winners)
+	} else {
+		filteredWinners, err := data.ListAllByYear(year)
+		if err != nil {
+			res.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		res.Write(filteredWinners)
+	}
 }
 
 // AddNewWinner adds new winner to the list
