390c7472d4cb774fc9d09e746b27881a0e5e6639
Vanaja Yakkala <vyakkala@vyakkala-a01.vmware.com>
2021-01-02 08:15:12 +0530

    Completed module

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -34,7 +34,16 @@ func ListWinners(res http.ResponseWriter, req *http.Request) {
 
 // AddNewWinner adds new winner to the list
 func AddNewWinner(res http.ResponseWriter, req *http.Request) {
-
+	accessToken := req.Header.Get("X-ACCESS-TOKEN")
+	if isTokenValid := data.IsAccessTokenValid(accessToken); !isTokenValid {
+		res.WriteHeader(http.StatusUnauthorized)
+	} else {
+		if err := data.AddNewWinner(req.Body); err != nil {
+			res.WriteHeader(http.StatusUnprocessableEntity)
+			return
+		}
+		res.WriteHeader(http.StatusCreated)
+	}
 }
 
 // WinnersHandler is the dispatcher for all /winners URL
